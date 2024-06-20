c278b13802a1545b3c5593dd02046c871cce952c
Kushagra  Srivastav <76401345+Kushagra7777@users.noreply.github.com>
2024-06-20 18:17:30 +0530

    Update netbox-device_discovery.py

--- Architectures/netbox-device_discovery.py
+++ Architectures/netbox-device_discovery.py
@@ -1,26 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-// aolabs.ai software >ao_core/Arch.py (C) 2023 Animo Omnis Corporation. All Rights Reserved.
-
-Thank you for your curiosity!
-"""
-
-
 ## // Netbox- Device Discovery -- Reference Design #2
+# Source: https://github.com/aolabsai/archs/blob/main/2_netbox-device_discovery.py
 # 
-# Our first practically useful Agent, a simple 40 neuron Agent (a 10-factor scale-up of our 4-neuron Basic Clam)
-# trained on instances of Netbox to predict Roles of new devices given local device configuration of live devices.
-# In simpler words, an Agent to answer the question, "what Role is this new device {Router, Patch Panel, Access Port} like to be given its info and the current devices we have?" 
-#
 # For interactive visual representation of this Arch:
-#    https://miro.com/app/board/uXjVM_kESvI=/?share_link_id=72701488535
+#    https://miro.com/app/live-embed/uXjVM_kESvI=/?moveToViewport=139623,-44894,15015,11297&embedId=159693252308
 
-
-description = "Netbox device type relational autocomplete (10 binary digits per field to encode ids)"
-arch_i = [10, 10, 10]               # a scaled up Basic Clam, with 3 input channels with have 10 neurons each, corresponding to device Mfg, Type, and Site (from IDss to 10-digit binary)
+description = "NetBox Device Discovery - a relational autocomplete"
+arch_i = [10, 10, 10]               # a scaled up Basic Clam, with 3 input channels with 10 neurons each, corresponding to device Mfg, Type, and Site (from IDss to 10-digit binary)
 arch_z = [10]                       # 10 neurons in 1 channel to encode device Role IDs
 arch_c = []
 connector_function = "forward_full_conn"
-
 # device mfg, type, and site are stored as strings (names) with associated unique IDs
 # using 10 binary neurons to encode integer IDs means we can encode up to 2^10 = 1048 unique binary values.
+
+# To maintain compatability with our API, do not change the variable name "Arch" or the constructor class "ao.Arch" in the line below (the API is pre-loaded with a version of the Arch class in this repo's main branch, hence "ao.Arch")
+Arch = ao.Arch(arch_i, arch_z, arch_c, connector_function, description)
+
+
+import requests
+url = "https://7svo9dnzu4.execute-api.us-east-2.amazonaws.com/v0dev/kennel"
+payload = {
+    "kennel_name": "NetBox Device Discovery TEST",  # give it a unique name
+    "arch_URL": "https://raw.githubusercontent.com/aolabsai/archs/main/2_netbox-device_discovery.py",
+    "description": "the simplest, atomic arch reference design, our hello, world",
+    "permissions": "free and open as the sea!"
+}
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "X-API-KEY": "weBuildBottomUpAGIsForAll"
+}
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.text)
